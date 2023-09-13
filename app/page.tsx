3977a706e62287cae171429e9d8e17d8889188d7
tequ <git@tequ.dev>
2023-09-13 23:30:10 +0900

    dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,102 @@
-import Image from 'next/image'
+'use client'
+import { Client, TransactionMetadata, dropsToXrp, isValidAddress } from 'xrpl'
+import BigNumber from 'bignumber.js'
+import { useRef, useState } from 'react'
+
+const getRoyalty = async (account: string) => {
+  const client = new Client('wss://xrpl.ws')
+  await client.connect()
+
+  const response = await client.requestAll({
+    command: 'account_tx',
+    account,
+    ledger_index_min: 75443457
+  })
+
+  const ModifiedNode = response.flatMap((r) =>
+    r.result.transactions.filter((tx) => {
+      return (tx.meta as any).TransactionResult === 'tesSUCCESS' && tx.tx?.Account !== account && tx.tx?.TransactionType === 'NFTokenAcceptOffer'
+    })
+  ).map((tx) => (tx.meta as TransactionMetadata).AffectedNodes.find((node,_,otherNodes) => {
+    if (!(node && 'ModifiedNode' in node)) {
+      return false
+    }
+    const isAccountRoot = node.ModifiedNode?.LedgerEntryType === 'AccountRoot'
+    const isUserAccount = node.ModifiedNode?.FinalFields?.Account === account
+    // check if this transaction affected the account's own XRP balance
+    const hasBalanceChanged = !!node.ModifiedNode.PreviousFields?.Balance
+    // check if this transaction affected the account's own NFToken to the offer
+    const hasDirectryNodeChanged = otherNodes.some((node) => {
+      if ('DeletedNode' in node) return 'DeletedNode' in node && node.DeletedNode.LedgerEntryType === 'DirectoryNode' && node.DeletedNode.FinalFields.Owner === account
+      if ('ModifiedNode' in node) return 'ModifiedNode' in node && node.ModifiedNode.LedgerEntryType === 'DirectoryNode' && node.ModifiedNode.FinalFields?.Owner === account
+      if ('CreatedNode' in node) return 'CreatedNode' in node && node.CreatedNode.LedgerEntryType === 'DirectoryNode' && node.CreatedNode.NewFields?.Owner === account
+      return false
+    })
+    return isAccountRoot && isUserAccount && hasBalanceChanged && !hasDirectryNodeChanged
+  })).filter((node)=> !!node)
+
+  console.log(ModifiedNode)
+  const xrplist = ModifiedNode.map((node) => {
+    if (node && 'ModifiedNode' in node ) {
+      return dropsToXrp(BigNumber(node.ModifiedNode.FinalFields?.Balance as number).minus(node.ModifiedNode.PreviousFields?.Balance as number).toNumber())
+    }
+    throw new Error('error')
+  })
+  
+  if(xrplist.length === 0) return '0'
+
+  const sum = xrplist.reduce((prev, current) => {
+    return BigNumber(prev).plus(current).toString()
+  })
+
+  await client.disconnect()
+  return sum
+}
 
 export default function Home() {
+  const [royalty, setRoyalty] = useState<string>('')
+  const addressRef = useRef<HTMLInputElement>(null)
+  const [loading,setLoading] = useState<boolean>(false)
+
+  const fetcher = async () => {
+    setLoading(true)
+    setRoyalty('')
+    try {
+      
+      const address = addressRef.current?.value
+      if (address === undefined || !isValidAddress(address)) {
+        alert('Invalid address')
+        setLoading(false)
+        return
+      }
+      const xrp = await getRoyalty(address)
+      setRoyalty(xrp)
+    } catch (e) {
+      console.error(e)
+      alert('Error')
+    } finally {
+      setLoading(false)      
+    }
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 max-w-5xl w-full text-3xl items-center justify-center font-mono lg:flex">
+        XRP Ledger Royalty Checker
+      </div>
+      {/* rnrJP7G4pPLKS5VQhVRmXKYkK2SEY2pC2X */}
+      <div className='input-group justify-center my-16'>
+        <input ref={addressRef} type="text" className='input w-[300px]' placeholder='rAbc...' />
+        <button className='btn btn-primary w-[70px]' onClick={fetcher} disabled={loading}>
+          {!loading ? 'Go': <span className="loading loading-spinner loading-xs"/>}
+        </button>
+      </div>
+
+      {royalty &&
+        <div className='text-center'>
+          <div>Total royalties you have received:</div>
+          <span className='text-2xl'>{royalty} XRP</span>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      }
     </main>
   )
 }
