4990432913c0e7d7e078388cad0dcc45c49331b8
tequ <git@tequ.dev>
2024-02-19 17:26:17 +0900

    add breakdown by taxon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Client, TransactionMetadata, dropsToXrp, isValidAddress } from 'xrpl'
+import { Client, NFTokenAcceptOffer, TransactionMetadata, dropsToXrp, isValidAddress, parseNFTokenID } from 'xrpl'
 import BigNumber from 'bignumber.js'
-import { useRef, useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 
 const getRoyalty = async (account: string) => {
   const client = new Client('wss://xrpl.ws')
@@ -17,52 +17,60 @@ const getRoyalty = async (account: string) => {
     r.result.transactions.filter((tx) => {
       return (tx.meta as any).TransactionResult === 'tesSUCCESS' && tx.tx?.Account !== account && tx.tx?.TransactionType === 'NFTokenAcceptOffer'
     })
-  ).map((tx) => (tx.meta as TransactionMetadata).AffectedNodes.find((node,_,otherNodes) => {
-    if (!(node && 'ModifiedNode' in node)) {
-      return false
-    }
-    const isAccountRoot = node.ModifiedNode?.LedgerEntryType === 'AccountRoot'
-    const isUserAccount = node.ModifiedNode?.FinalFields?.Account === account
-    // check if this transaction affected the account's own XRP balance
-    const hasBalanceChanged = !!node.ModifiedNode.PreviousFields?.Balance
-    // check if this transaction affected the account's own NFToken to the offer
-    const hasDirectryNodeChanged = otherNodes.some((node) => {
-      if ('DeletedNode' in node) return 'DeletedNode' in node && node.DeletedNode.LedgerEntryType === 'DirectoryNode' && node.DeletedNode.FinalFields.Owner === account
-      if ('ModifiedNode' in node) return 'ModifiedNode' in node && node.ModifiedNode.LedgerEntryType === 'DirectoryNode' && node.ModifiedNode.FinalFields?.Owner === account
-      if ('CreatedNode' in node) return 'CreatedNode' in node && node.CreatedNode.LedgerEntryType === 'DirectoryNode' && node.CreatedNode.NewFields?.Owner === account
-      return false
-    })
-    return isAccountRoot && isUserAccount && hasBalanceChanged && !hasDirectryNodeChanged
-  })).filter((node)=> !!node)
+  ).map((tx) => ({
+    node: (tx.meta as TransactionMetadata).AffectedNodes.find((node, _, otherNodes) => {
+      if (!(node && 'ModifiedNode' in node)) {
+        return false
+      }
+      const isAccountRoot = node.ModifiedNode?.LedgerEntryType === 'AccountRoot'
+      const isUserAccount = node.ModifiedNode?.FinalFields?.Account === account
+      // check if this transaction affected the account's own XRP balance
+      const hasBalanceChanged = !!node.ModifiedNode.PreviousFields?.Balance
+      // check if this transaction affected the account's own NFToken to the offer
+      const hasDirectryNodeChanged = otherNodes.some((node) => {
+        if ('DeletedNode' in node) return 'DeletedNode' in node && node.DeletedNode.LedgerEntryType === 'DirectoryNode' && node.DeletedNode.FinalFields.Owner === account
+        if ('ModifiedNode' in node) return 'ModifiedNode' in node && node.ModifiedNode.LedgerEntryType === 'DirectoryNode' && node.ModifiedNode.FinalFields?.Owner === account
+        if ('CreatedNode' in node) return 'CreatedNode' in node && node.CreatedNode.LedgerEntryType === 'DirectoryNode' && node.CreatedNode.NewFields?.Owner === account
+        return false
+      })
+      return isAccountRoot && isUserAccount && hasBalanceChanged && !hasDirectryNodeChanged
+    }),
+    NFTokenID: (tx.meta as TransactionMetadata<NFTokenAcceptOffer>).nftoken_id
+  })).filter((node) => !!node.node)
 
   console.log(ModifiedNode)
   const xrplist = ModifiedNode.map((node) => {
-    if (node && 'ModifiedNode' in node ) {
-      return dropsToXrp(BigNumber(node.ModifiedNode.FinalFields?.Balance as number).minus(node.ModifiedNode.PreviousFields?.Balance as number).toNumber())
+    if (node.node && 'ModifiedNode' in node.node) {
+      const finalBalance = node.node.ModifiedNode.FinalFields?.Balance
+      const previousBalance = node.node.ModifiedNode.PreviousFields?.Balance
+      return {
+        taxon: parseNFTokenID(node.NFTokenID!).Taxon,
+        amount: dropsToXrp(BigNumber(finalBalance as number).minus(previousBalance as number).toNumber())
+      }
     }
     throw new Error('error')
   })
-  
-  if(xrplist.length === 0) return '0'
+
+  if (xrplist.length === 0) return {}
 
   const sum = xrplist.reduce((prev, current) => {
-    return BigNumber(prev).plus(current).toString()
-  })
+    const prevValue = prev[String(current.taxon)] || 0
+    return { ...prev, [current.taxon]: BigNumber(prevValue).plus(current.amount).toString() }
+  }, {} as { [key: string]: string })
 
   await client.disconnect()
   return sum
 }
 
 export default function Home() {
-  const [royalty, setRoyalty] = useState<string>('')
+  const [royalty, setRoyalty] = useState<{ [key in string]: string }>({})
   const addressRef = useRef<HTMLInputElement>(null)
-  const [loading,setLoading] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const fetcher = async () => {
     setLoading(true)
-    setRoyalty('')
+    setRoyalty({})
     try {
-      
       const address = addressRef.current?.value
       if (address === undefined || !isValidAddress(address)) {
         alert('Invalid address')
@@ -75,9 +83,12 @@ export default function Home() {
       console.error(e)
       alert('Error')
     } finally {
-      setLoading(false)      
+      setLoading(false)
     }
   }
+
+  const royaltySum = useMemo(() => Object.values(royalty).reduce((prev, current) => BigNumber(prev).plus(current).toString(), '0'), [royalty])
+
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full text-3xl items-center justify-center font-mono lg:flex">
@@ -87,14 +98,23 @@ export default function Home() {
       <div className='input-group justify-center my-16'>
         <input ref={addressRef} type="text" className='input w-[300px]' placeholder='rAbc...' />
         <button className='btn btn-primary w-[70px]' onClick={fetcher} disabled={loading}>
-          {!loading ? 'Go': <span className="loading loading-spinner loading-xs"/>}
+          {!loading ? 'Go' : <span className="loading loading-spinner loading-xs" />}
         </button>
       </div>
 
-      {royalty &&
+      {Object.keys(royalty).length > 0 &&
         <div className='text-center'>
           <div>Total royalties you have received:</div>
-          <span className='text-2xl'>{royalty} XRP</span>
+          <span className='text-2xl'>{royaltySum} XRP</span>
+
+          <div className='mt-4'>
+            <span className='text-xl'>Breakdown:</span>
+            <div className='text-left px-2'>
+              {Object.entries(royalty).map(([key, value]) => (
+                <div key={key} className='flex justify-between'>Taxon {key}: <span>{parseFloat(value).toFixed(6)} XRP</span></div>
+              ))}
+            </div>
+          </div>
         </div>
       }
     </main>
